12792d66bb6f5d1e48d60c74546e47bea2a99070
Ainun Abdullah <abdullah.ainun4@gmail.com>
2025-02-01 10:33:16 +0700

    feat: add module virtual machine with ubuntu 2404

---
+++ virtual-machine/modules/virtual-machine-with-ubuntu-2404/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  default     = "vm1"
+  type        = string
+  description = "virtual machine name"
+}
+
+
+variable "node_name" {
+  default     = "pve1"
+  type        = string
+  description = "proxmox node name"
+}
+
+variable "vm_id" {
+  default     = 1234
+  type        = number
+  description = "proxmox virtual machine id"
+}
+
+variable "file_id" {
+  default     = "nfs-repo:iso/ubuntu-24.04.1-live-server-amd64.iso" # The ID format is <datastore_id>:<content_type>/<file_name>
+  type        = string
+  description = "proxmox file id"
+}

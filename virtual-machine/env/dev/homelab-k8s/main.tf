6bbaf70e8d1d777f342727be7badd1c882327b3a
Ainun Abdullah <abdullah.ainun4@gmail.com>
2025-03-06 04:00:43 +0000

    feat:  add backend config

--- virtual-machine/env/dev/homelab-k8s/main.tf
+++ virtual-machine/env/dev/homelab-k8s/main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 0.14"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://minio-api.abdullahainun.site" # Minio endpoint
+    }
+    bucket = "homelab-tf-proxmox"
+    key    = "state/virtual-machine/homelab-k8s"
+
+    region                      = "main" # Region validation will be skipped
+    skip_credentials_validation = true   # Skip AWS related checks and validations
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true # Enable path-style S3 URLs (https://<HOST>/<BUCKET> https://developer.hashicorp.com/terraform/language/settings/backends/s3#use_path_style
+  }
+}
+
 module "homelab_k8s_cluster" {
   source  = "../../../modules/virtual-machine-with-talos/"
   proxmox = var.proxmox
